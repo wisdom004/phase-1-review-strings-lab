30aae763b4a05c32491b0748cd766714f5e02a39
Ian <ian.njoroge2@student.moringaschool.com>
2025-03-05 23:37:36 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser='Grace Hopper';
+const welcomeMessage='Welcome to Flatbook, Grace Hopper!'
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${G.slice(0, 1)}!'
